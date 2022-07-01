c6b14b1e4a95a0e024f903de303e90974d489340
Maciej Krolikowski <mkkrolik@gmail.com>
2022-07-01 13:12:20 +0200

    Update saleforcesdk to v10.1

--- SalesforceSDKCore.xcframework/ios-arm64/SalesforceSDKCore.framework/Headers/SalesforceSDKConstants.h
+++ SalesforceSDKCore.xcframework/ios-arm64/SalesforceSDKCore.framework/Headers/SalesforceSDKConstants.h
@@ -61,7 +61,9 @@
 
 #define __SALESFORCE_SDK_10_0_0 100000
 
-#define SALESFORCE_SDK_VERSION_MIN_REQUIRED __SALESFORCE_SDK_10_0_0
+#define __SALESFORCE_SDK_10_1_0 100100
+
+#define SALESFORCE_SDK_VERSION_MIN_REQUIRED __SALESFORCE_SDK_10_1_0
 
 #define SALESFORCE_SDK_VERSION [NSString stringWithFormat:@"%d.%d.%d%@",              \
                                 (SALESFORCE_SDK_VERSION_MIN_REQUIRED / 10000),        \
